b734259c4242c8c52cdd7c673f3f7cfaac8a3234
lxymoon <73787672+lxymoon@users.noreply.github.com>
2021-09-09 12:08:59 +0800

    Delete Sparrow (1) directory

--- Sparrow (1)/index/search.js
+++
@@ -1,7 +0,0 @@
-function search(){
-    if(document.getElementById("search_input").value!=""){
-        window.location.href="https://baidu.com/baidu?wd="+document.getElementById("search_input").value;
-        document.getElementById("search_input").value="";
-    }
-    return false;
-}
